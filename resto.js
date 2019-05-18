a9f76d8d1708eeb18e6a42739d23ca65ca320bc5
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-05-18 11:18:39 -0400

    test

--- resto.js
+++ resto.js
@@ -4,8 +4,8 @@
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -13,10 +13,10 @@ var path = require("path");
 // ==============================================================================
 
 // Tells node that we are creating an "express" server
-var app = express();
+const app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
